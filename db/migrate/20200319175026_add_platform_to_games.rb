0d7e9a777d1ad7383f36c54b442370f3c335c4cc
catalingriu <catalin.griu1@gmail.com>
2020-05-09 14:47:26 +0300

    heroku config2

--- db/migrate/20200319175026_add_platform_to_games.rb
+++
@@ -1,5 +0,0 @@
-class AddPlatformToGames < ActiveRecord::Migration[6.0]
-  def change
-    add_column :games, :platform, :string
-  end
-end
--- db/migrate/20200320115932_change_platform_to_be_array_in_games.rb
+++
@@ -1,5 +0,0 @@
-class ChangePlatformToBeArrayInGames < ActiveRecord::Migration[6.0]
-  def change
-    change_column :games, :platform, :string, default: [].to_yaml, array: true
-  end
-end
