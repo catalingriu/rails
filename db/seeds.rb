06c528cad5045b480e3fd94867c0a8cffb0a373e
catalingriu <catalin.griu1@gmail.com>
2020-05-10 10:43:22 +0300

    editseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,16 @@
    # Game.create!({name: 'DOTA 4', platform_id: platforms[0].id, quantity: 1, loan_period: 1})
   user = User.create! email: 'johnjohn@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', isadmin: 2
   platforms = Platform.create!([{name:'PC'}, {name:'PS'}, {name: 'Xbox'}])
+  
 
+  loans = Loan.create!([
+     {}
+  ])
   games = Game.create!([
-     {name: "Grand Theft Auto V", quantity: 997, year: 2013, trailer: "VjZ5tgjPVfU", loan_period: 5, platform: platforms[0] },
-     {name: "Counter-Strike: Global Offensive", quantity: 999, year: 2010, trailer: "edYCtaNueQY", loan_period: 5,  platform: platforms[0]},
-     {name: "Need for Speed: Most Wanted", quantity: 999, year: 2005, trailer: "DEmRkNnCvLU", loan_period: 5,  platform: platforms[0]},
-     {name: "DOTA 2", quantity: 976, year: 20133, trailer: "-cSFPIwMEq4", loan_period: 5,  platform: platforms[1]},
-     {name: "Mafia III", quantity: 1, year: 2016, trailer: "TNiHOpHbCzM", loan_period: 10,  platform: platforms[2]},
-     {name: "Call of Duty: Modern Warfare", quantity: 2, year: 2019, trailer: "bH1lHCirCGI", loan_period: 5,  platform: platforms[2]}
+     {name: "Grand Theft Auto V", quantity: 997, year: 2013, trailer: "VjZ5tgjPVfU", loan_period: 5, platforms: platforms[0,1] },
+     {name: "Counter-Strike: Global Offensive", quantity: 999, year: 2010, trailer: "edYCtaNueQY", loan_period: 5,  platforms: platforms[0,2]},
+     {name: "Need for Speed: Most Wanted", quantity: 999, year: 2005, trailer: "DEmRkNnCvLU", loan_period: 5,  platforms: platforms[0,3]},
+     {name: "DOTA 2", quantity: 976, year: 20133, trailer: "-cSFPIwMEq4", loan_period: 5,  platforms: platforms[1,1]},
+     {name: "Mafia III", quantity: 1, year: 2016, trailer: "TNiHOpHbCzM", loan_period: 10,  platforms: platforms[2,1]},
+     {name: "Call of Duty: Modern Warfare", quantity: 2, year: 2019, trailer: "bH1lHCirCGI", loan_period: 5,  platforms: platforms[2, 1]}
   ])
