0b4f162508f468b58c77ad1947bfcec46b023bde
catalingriu <catalin.griu1@gmail.com>
2020-05-10 09:46:02 +0300

    dbseednw

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+   # Game.create!({name: 'DOTA 4', platform_id: platforms[0].id, quantity: 1, loan_period: 1})
+  # user = User.create! email: 'johnjohn@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', isadmin: 2
+  platforms = Platform.create!([{name:'PQ'}, {name:'pw'}])
